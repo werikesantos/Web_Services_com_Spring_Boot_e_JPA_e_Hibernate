d5c7a855ee38d315953cb65d658934d87975f04f
werikesantos <werike.ms@gmail.com>
2021-07-22 18:57:01 -0300

    OBS: TESTE REALIZADO NA PORTA '...8080/usuario' RETORNANDO O OBJETO NO PADRÃO JSON

---
+++ projeto/src/main/java/com/werike/projeto/entidades/Usuario.java
@@ -0,0 +1,80 @@
+package com.werike.projeto.entidades;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Usuario implements Serializable{//Serializable => SERVE PARA O OBJETO TRAFEGE NA REDE E POSSA SER GRAVADO EM ARQUIVOS
+	private static final long serialVersionUID = 1L; //É UM NÚMERO DE SERIE PADRÃO
+	
+	private Long id;
+	private String name;
+	private String email;
+	private String telefone;
+	private String senha;
+	
+	public Usuario() {
+	}
+	
+	public Usuario(Long id, String name, String email, String telefone, String senha) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.telefone = telefone;
+		this.senha = senha;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	@Override
+	public String toString() {
+		return "Usuarios [id=" + id + ", name=" + name + ", email=" + email + ", telefone=" + telefone + ", senha="
+				+ senha + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+}
